77c3276bffb953d1678bc0cd156293739d31734f
Viktor Huchiev <274128020+viktorHuc@users.noreply.github.com>
2026-06-01 11:51:03 -0700

    Traversal practice

--- src/Traversal.java
+++ src/Traversal.java
@@ -1,5 +1,48 @@
 public class Traversal {
   public static void main(String[] args) {
+    TreeNode root = new TreeNode(10);
+    root.left = new TreeNode(9);
+    root.left.left = new TreeNode(5);
+    root.left.right = new TreeNode(2);
+
+    root.right = new TreeNode(15, 
+                                      new TreeNode(-3), 
+                                      new TreeNode(5, 
+                                                  null, 
+                                                  new TreeNode(22)
+                                                )
+                              );
+    inorder(root);
+  }
+
+  public static void preorder(TreeNode current){
+    if(current == null ) return;
+
+    System.out.println(current.value);
+
+    //explore left
+    preorder(current.left);
+    preorder(current.right);
 
   }
+
+  public static void postorder(TreeNode current){
+    if(current == null ) return;
+
+    //explore left
+    postorder(current.left);
+    postorder(current.right);
+
+    System.out.println(current.value);
+  }
+  public static void inorder(TreeNode current){
+    if(current == null ) return;
+
+    //explore left
+    inorder(current.left);
+    System.out.println(current.value);
+    inorder(current.right);
+  }
 }
+
+  
--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,4 +1,16 @@
 public class TreeNode {
+    public int value;
+    public TreeNode left;
+    public TreeNode right;
 
+    public TreeNode(int value, TreeNode left, TreeNode right){
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+    
+    public TreeNode(int value) {
+        this.value = value;
+    }
   
 }
